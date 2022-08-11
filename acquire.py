50d0658957b410f43d20880a8ebffca0dd872c15
kevin-mal-smith <kevin.mal.smith@gmail.com>
2022-08-11 13:26:33 -0400

    exercise push

---
+++ acquire.py
@@ -0,0 +1,169 @@
+import pandas as pd
+import numpy as np
+import os
+from env import host, username, password
+
+
+def get_connection(db, user=username, host=host, password=password):
+    
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+    
+    
+    
+def new_titanic_data():
+  
+    # Create SQL query.
+    sql_query = 'SELECT * FROM passengers'
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('titanic_db'))
+    
+    return df
+
+
+
+def get_titanic_data():
+   
+    if os.path.isfile('titanic_df.csv'):
+        
+        # If csv file exists, read in data from csv file.
+        df = pd.read_csv('titanic_df.csv', index_col=0)
+        
+    else:
+        
+        # Read fresh data from db into a DataFrame.
+        df = new_titanic_data()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('titanic_df.csv')
+        
+    return df
+
+
+
+def new_iris_data():
+    
+    sql_query = """
+                SELECT 
+                    species_id,
+                    species_name,
+                    sepal_length,
+                    sepal_width,
+                    petal_length,
+                    petal_width
+                FROM measurements
+                JOIN species USING(species_id)
+                """
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('iris_db'))
+    
+    return df
+
+
+def get_iris_data():
+   
+    if os.path.isfile('iris_df.csv'):
+        
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('iris_df.csv', index_col=0)
+        
+    else:
+        
+        # Read fresh data from db into a DataFrame
+        df = new_iris_data()
+        
+        # Cache data
+        df.to_csv('iris_df.csv')
+        
+    return df
+
+def new_telco_data():
+   
+    sql_query = """
+                select * from customers
+                join contract_types using (contract_type_id)
+                join internet_service_types using (internet_service_type_id)
+                join payment_types using (payment_type_id)
+                """
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('telco_churn'))
+    
+    return df
+
+def get_telco_data():
+    
+    if os.path.isfile('telco.csv'):
+        
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('telco.csv', index_col=0)
+        
+    else:
+        
+        # Read fresh data from db into a DataFrame
+        df = new_telco_data()
+        
+        # Cache data
+        df.to_csv('telco.csv')
+        
+    return df
+
+
+
+def new_zillow_data():
+   
+    sql_query = '''
+        SELECT prop.*,
+        predictions_2017.logerror,
+        predictions_2017.transactiondate,
+        air.airconditioningdesc,
+        arch.architecturalstyledesc,
+        build.buildingclassdesc,
+        heat.heatingorsystemdesc,
+        land.propertylandusedesc,
+        story.storydesc,
+        type.typeconstructiondesc
+        FROM properties_2017 prop
+        JOIN (
+            SELECT parcelid, MAX(transactiondate) AS max_transactiondate
+            FROM predictions_2017
+            GROUP BY parcelid
+            ) pred USING(parcelid)
+        JOIN predictions_2017 ON pred.parcelid = predictions_2017.parcelid
+                          AND pred.max_transactiondate = predictions_2017.transactiondate
+        LEFT JOIN airconditioningtype air USING(airconditioningtypeid)
+        LEFT JOIN architecturalstyletype arch USING(architecturalstyletypeid)
+        LEFT JOIN buildingclasstype build USING(buildingclasstypeid)
+        LEFT JOIN heatingorsystemtype heat USING(heatingorsystemtypeid)
+        LEFT JOIN propertylandusetype land USING(propertylandusetypeid)
+        LEFT JOIN storytype story USING(storytypeid)
+        LEFT JOIN typeconstructiontype type USING(typeconstructiontypeid)
+        WHERE propertylandusedesc = "Single Family Residential"
+            AND transactiondate <= '2017-12-31'
+            AND prop.longitude IS NOT NULL
+            AND prop.latitude IS NOT NULL
+        '''
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('telco_churn'))
+    
+    return df
+
+def zillow_data():
+    
+    if os.path.isfile('telco.csv'):
+        
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('telco.csv', index_col=0)
+        
+    else:
+        
+        # Read fresh data from db into a DataFrame
+        df = new_telco_data()
+        
+        # Cache data
+        df.to_csv('telco.csv')
+        
+    return df
+
